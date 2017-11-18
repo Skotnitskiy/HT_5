365df48b497b281ef9a866dace137ff4787cc346
serhSK <sergeyskotnitskiy@gmail.com>
2017-11-19 00:51:08 +0200

    create results dir

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
 categories_list = ["skstories", "showstories", "newstories", "jobstories"]
 default_categorie = "newstories"
+results_path = "../results/"
+rep_file_name = "report.csv"
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import argparse
+import os
 import config as conf
 
 parser = argparse.ArgumentParser(description='Data parse.')
 parser.add_argument('-c', action="store", dest="categorie", default=conf.default_categorie,
                     choices=conf.categories_list)
+# create dir
+if not os.path.exists(conf.results_path):
+    os.mkdir(conf.results_path)
 
+# create report.csv if not exists
+if not os.path.exists(conf.results_path + conf.rep_file_name):
+    f = open(conf.results_path + conf.rep_file_name, "w")
