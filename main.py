76c4a7c0d469a7017815b73b91d57b20882d0fbf
serhSK <sergeyskotnitskiy@gmail.com>
2017-11-19 01:33:07 +0200

    added logging create file report.csv and dir results

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import argparse
 import os
+import logging.config
+
 import config as conf
+
+logging.config.dictConfig(conf.dictLogConfig)
+logger = logging.getLogger("DataParserApp")
+logger.info("Program started")
 
 parser = argparse.ArgumentParser(description='Data parse.')
 parser.add_argument('-c', action="store", dest="categorie", default=conf.default_categorie,
@@ -8,7 +14,13 @@ parser.add_argument('-c', action="store", dest="categorie", default=conf.default
 # create dir
 if not os.path.exists(conf.results_path):
     os.mkdir(conf.results_path)
+    logger.info("results dir created")
+else:
+    logger.info("results directory already exists")
 
 # create report.csv if not exists
 if not os.path.exists(conf.results_path + conf.rep_file_name):
     f = open(conf.results_path + conf.rep_file_name, "w")
+    logger.info("file", conf.rep_file_name, "created")
+else:
+    logger.info("file " + conf.rep_file_name + " already exists")
